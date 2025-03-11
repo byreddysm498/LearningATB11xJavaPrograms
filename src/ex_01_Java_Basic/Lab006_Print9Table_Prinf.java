6925c7873d84f9f5621d5b813fb8113638065b31
byreddysm498 <byreddysm498@gmail.com>
2025-03-11 18:48:07 +0530

    Lab006_Print9Table_Prinf

---
+++ src/ex_01_Java_Basic/Lab006_Print9Table_Prinf.java
@@ -0,0 +1,25 @@
+package ex_01_Java_Basic;
+
+public class Lab006_Print9Table_Prinf {
+    public static void main(String[] args){
+
+        int i = 9;
+        int in = 1;
+        System.out.println("The table of 9 by using printf");
+        System.out.println("-------------------------------");
+        System.out.printf("9*%d = %d\n",in,i);
+
+        System.out.printf("9*%d = %d\n",in=in+1,i=i+9);
+        System.out.printf("9*%d = %d\n",in=in+1,i=i+9);
+        System.out.printf("9*%d = %d\n",in=in+1,i=i+9);
+        System.out.printf("9*%d = %d\n",in=in+1,i=i+9);
+        System.out.printf("9*%d = %d\n",in=in+1,i=i+9);
+        System.out.printf("9*%d = %d\n",in=in+1,i=i+9);
+        System.out.printf("9*%d = %d\n",in=in+1,i=i+9);
+        System.out.printf("9*%d = %d\n",in=in+1,i=i+9);
+        System.out.printf("9*%d = %d\n",in=in+1,i=i+9);
+
+
+
+    }
+}
